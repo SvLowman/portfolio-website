b87115d8ada9cf14aa42419e446702e903e19271
SvLowman <SvenLohmann@gmx.de>
2021-01-29 23:09:29 +0100

    :sparkles: Create structure of CV page

--- src/content/content.js
+++ src/content/content.js
@@ -54,3 +54,42 @@ export const techStackContent = [
     name: "Mongo DB",
   },
 ];
+
+export const cvContent = [
+  {
+    position: "Trainee Webentwicklung",
+    company: "neue fische - Digital School and Pool for Talent, Köln",
+    period: "09 / 2020 - 01 / 2021",
+    tasks: [
+      "Intensiv-Coding-Bootcamp in Vollzeit (540 Stunden)",
+      "Entwicklung einer eigenen WebApp",
+      "Erlernen von HTML, CSS, JavaScript, React",
+    ],
+  },
+  {
+    position: "Lehrkraft",
+    company: "Sächsische Lehmbaugruppe, Leipzig",
+    period: "02 / 2019 - 08 / 2020",
+    tasks: [
+      "Planung und Durchführung von Unterricht in Grundbildungsfächer für Teilnehmer an einem Schulersatzprojekt",
+    ],
+  },
+  {
+    position: "Digital Trainer",
+    company: "Capita Customer Services, Leipzig",
+    period: "11 / 2017 - 11 / 2018",
+    tasks: ["Konzeption und Betextung von digitalen Lernumgebungen"],
+  },
+  {
+    position: "Redaktionsmitarbeiter",
+    company: "Infonetwork GmbH, Mediengrupe RTL, Köln",
+    period: "07 / 2012 - 07 / 2016",
+    tasks: ["Erstellen von Nachrichtenbeiträgen"],
+  },
+  {
+    position: "Promotionsstudium Deutsche Philologie",
+    company: "Universität zu Köln, Istitut für deutsche Sprache und Literatur",
+    period: "10 / 2010 - 10 / 2016",
+    tasks: ["Abschluss: Dr. phil."],
+  },
+];
--- src/pages/CVPage.js
+++ src/pages/CVPage.js
@@ -1,6 +1,19 @@
 import React from "react";
-// import styled from "styled-components/macro";
+import styled from "styled-components/macro";
 import { Container, TextContainer } from "../components/Container";
+import { cvContent } from "../content/content";
+
+const CVContainer = styled.div`
+  border: solid 1px black;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+const CVCard = styled.div`
+  border: solid 1px black;
+  width: fit-content;
+`;
 
 const CVPage = () => (
   <Container>
@@ -20,6 +33,20 @@ const CVPage = () => (
         Tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Semper
         feugiat nibh sed pulvinar proin.
       </p>
+      <CVContainer>
+        {cvContent.map((object, index) => (
+          <CVCard key={index}>
+            <p>{object.position}</p>
+            <p>{object.company}</p>
+            <p>{object.period}</p>
+            <ul>
+              {object.tasks.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </CVCard>
+        ))}
+      </CVContainer>
     </TextContainer>
   </Container>
 );
