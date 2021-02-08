2db4f95a256c725dbbacb8b68d6ffac129cc0176
SvLowman <SvenLohmann@gmx.de>
2021-02-08 08:52:56 +0100

    :lipstick: Implement fonts

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -11,7 +11,11 @@ const GlobalStyle = createGlobalStyle`
 body {
   margin: 0;
   font-size: 16px;
+  font-family: 'Montserrat', sans-serif;
 }
 `;
+
+// font-family: 'Montserrat', sans-serif;
+// font-weight: 400 (running text) / 500 (smaller titles) / 700 (titles) / 800 (small category caption);
 
 export default GlobalStyle;
