404031ca503cb2fc1e542201a2a237e8f4320aaa
SvLowman <SvenLohmann@gmx.de>
2021-02-17 22:21:01 +0100

    :speech_balloon: Make splash page blank until further notice

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,17 +1,7 @@
 import React from "react";
 // import styled from "styled-components/macro";
-import { Container, TextContainer } from "../components/Container";
+import { Container } from "../components/Container";
 
-const LandingPage = () => (
-  <Container>
-    <TextContainer>
-      <h3>Hello World 😀!</h3>
-      <p>
-        Hier auf meiner Portfolio-Seite findet sich ein kleiner Überblick über
-        bisherige Projekte und Qualifikationen.
-      </p>
-    </TextContainer>
-  </Container>
-);
+const LandingPage = () => <Container></Container>;
 
 export default LandingPage;
